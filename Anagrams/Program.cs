a76b9e9d1862c5247ee3edb7fdaf4f12f9298d74
bogdansa <bogdan.sajewicz@gmail.com>
2019-05-23 02:23:03 +0200

    Created 'Anagrams' solution

--- Anagrams/Program.cs
+++ Anagrams/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Anagrams
 {
     class Program
     {
+        private const string defaultFileName = @"..\..\Anagrams.txt";
+
         static void Main(string[] args)
         {
+            string[] inputWords = System.IO.File.ReadAllLines(defaultFileName);
+
+            var anagramSets = inputWords.GroupBy(word => String.Concat(word.OrderBy(character => character)))
+                .OrderByDescending(set => set.First().Count())
+                .OrderByDescending(set => set.Count());
+
+            foreach (var set in anagramSets)
+            {
+                Console.WriteLine(string.Join(" ", set.OrderBy(word => word)));
+            }
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
         }
     }
 }
